2185e866f2d3faff7d66f2ed38dc7e912c41f992
AakashNarwade <aakashnarwade06@gmail.com>
2023-02-21 14:48:59 +0530

    mailList component

--- frontend/src/components/mailList/MailList.jsx
+++ frontend/src/components/mailList/MailList.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./mailList.css";
 
 const MailList = () => {
-  return <div className="mailListContainer">MailList</div>;
+  return (
+    <div className="mail">
+      <h1 className="mailTitle">Save time, save money!</h1>
+      <span className="mailDesc">Signup and we'll send you an email</span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="Your Email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default MailList;
